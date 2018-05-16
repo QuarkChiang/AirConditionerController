7e08dc1bebcd1d2cdab7b0d4ce736d432032d9cf
UShicyan <k456852789@hotmail.com>
2018-05-16 19:46:31 +0800

    Init Data

---
+++ AirConditionerController.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		AirConditionerController.ino
+ Created:	2018/5/16 下午 07:45:23
+ Author:	UShi
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
